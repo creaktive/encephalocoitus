df01efb860d628275e439433e1b804e1d045e17d
Stanislaw Pusep <creaktive@gmail.com>
2012-12-12 14:21:46 -0200

    optimized

--- bf.pl
+++ bf.pl
@@ -3,11 +3,11 @@ use strict;
 use utf8;
 use warnings qw(all);
 
-use constant WORD_SIZE => 8;
-use constant DATA_SIZE => 65536;
+sub brainfuck ($;$) {
+    my ($program, $WORD_SIZE) = (@_, 8);
 
-sub brainfuck ($) {
-    my ($program) = @_;
+    use bytes;
+    use integer;
 
     local ($|, $/) = (1, \1);
 
@@ -18,15 +18,16 @@ sub brainfuck ($) {
     my $op = {
         q(>) => sub { ++$si },
         q(<) => sub { --$si },
-        q(+) => sub { ++vec $data, $si, WORD_SIZE },
-        q(-) => sub { --vec $data, $si, WORD_SIZE },
-        q(.) => sub { print STDOUT chr vec $data, $si, WORD_SIZE },
-        q(,) => sub { vec($data, $si, WORD_SIZE) = ord <STDIN> },
-        q([) => sub { $ip = vec($data, $si, WORD_SIZE) ? $ip : $end{$ip} },
+        q(+) => sub { ++vec $data, $si, $WORD_SIZE },
+        q(-) => sub { --vec $data, $si, $WORD_SIZE },
+        q(.) => sub { print STDOUT chr vec $data, $si, $WORD_SIZE },
+        q(,) => sub { vec($data, $si, $WORD_SIZE) = ord <STDIN> },
+        q([) => sub { $ip = vec($data, $si, $WORD_SIZE) ? $ip : $end{$ip} },
         q(]) => sub { $ip = $start{$ip} - 1 },
     };
 
-    my @code = grep { exists $op->{$_} } split // => $program;
+    $program =~ s/[^\Q@{[ keys %$op ]}\E]//gox;
+    my @code = grep { exists $op->{$_} } split //x => $program;
 
     my @stack;
     for my $i (0 .. $#code) {
@@ -37,36 +38,67 @@ sub brainfuck ($) {
         }
     }
 
+    my $c = 0;
     while ($ip <= $#code) {
         $op->{$code[$ip]}->();
-        $si %= DATA_SIZE;
     } continue {
+        ++$c;
         ++$ip;
     }
+
+    return $c;
 }
 
 use Benchmark;
 
 timethis 100 => sub {
-    brainfuck q(++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.);
+    # Hello World!
+    printf qq(%d\n), brainfuck
+        q(++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.);
 
-    brainfuck q(
-    +++++++++++
-    >+>>>>++++++++++++++++++++++++++++++++++++++++++++
-    >++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>
-    +<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-
-    <-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<
-    -]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]
-    >[<<+>>[-]]<<<<<<<]>>>>>[+++++++++++++++++++++++++
-    +++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++
-    ++++++++++++++++++++++++++++++++++++++++++++.[-]<<
-    <<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<
-    [-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]
-    );
-
-    print "\n";
+    # 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+    printf qq(\n%d\n), brainfuck << 'FIBONACCI';
+        +++++++++++
+        >+>>>>++++++++++++++++++++++++++++++++++++++++++++
+        >++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>
+        +<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-
+        <-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<
+        -]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]
+        >[<<+>>[-]]<<<<<<<]>>>>>[+++++++++++++++++++++++++
+        +++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++
+        ++++++++++++++++++++++++++++++++++++++++++++.[-]<<
+        <<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<
+        [-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]
+FIBONACCI
 };
 
-__DATA__
-timethis 100: 12 wallclock secs (12.14 usr +  0.03 sys = 12.17 CPU) @  8.22/s (n=100)
-timethis 100: 15 wallclock secs (15.11 usr +  0.03 sys = 15.14 CPU) @  6.61/s (n=100)
+brainfuck << 'ROT13';
+-,+[                        Read first character and start outer character reading loop
+    -[                      Skip forward if character is 0
+        >>++++[>++++++++<-] Set up divisor (32) for division loop
+                                (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
+        <+<-[               Set up dividend (x minus 1) and enter division loop
+            >+>+>-[>>>]     Increase copy and remainder / reduce divisor / Normal case: skip forward
+            <[[>+<-]>>+>]   Special case: move remainder back to divisor and increase quotient
+            <<<<<-          Decrement dividend
+        ]                   End division loop
+    ]>>>[-]+                End skip loop; zero former divisor and reuse space for a flag
+    >--[-[<->+++[-]]]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
+        ++++++++++++<[      If flag then set up divisor (13) for second division loop
+                                (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
+            >-[>+>>]        Reduce divisor; Normal case: increase remainder
+            >[+[<+>-]>+>>]  Special case: increase remainder / move it back to divisor / increase quotient
+            <<<<<-          Decrease dividend
+        ]                   End division loop
+        >>[<+>-]            Add remainder back to divisor to get a useful 13
+        >[                  Skip forward if quotient was 0
+            -[              Decrement quotient and skip forward if quotient was 1
+                -<<[-]>>    Zero quotient and divisor if quotient was 2
+            ]<<[<<->>-]>>   Zero divisor and subtract 13 from copy if quotient was 1
+        ]<<[<<+>>-]         Zero divisor and add 13 to copy if quotient was 0
+    ]                       End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
+    <[-]                    Clear remainder from first division if second division was skipped
+    <.[-]                   Output ROT13ed character from copy and clear it
+    <-,+                    Read next character
+]                           End character reading loop
+ROT13
