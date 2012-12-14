df90c832cff13a93b61119daccf73a1b388cd3e0
Stanislaw Pusep <creaktive@gmail.com>
2012-12-14 12:48:10 -0200

    perl translator

--- bf.pl
+++ bf.pl
@@ -4,24 +4,106 @@ use strict;
 use utf8;
 use warnings qw(all);
 
-use Carp qw(confess);
+use B::Deparse;
+use Benchmark qw(:hireswallclock);
+use Carp qw(confess croak);
+use Data::Dumper;
+use PadWalker;
 
-sub brainfuck ($;$) {
-    my ($program, $WORD_SIZE) = (@_, 8);
+my ($hint_bits, $warning_bits, $hinthash);
+
+sub brainfuck2perl {
+    my ($code) = @_;
 
     use bytes;
     use integer;
 
-    local ($|, $/) = (1, \1);
-    my ($data, $si) = ('', 0);
+    my @buffer = (
+        q(#!/usr/bin/perl),
+        q(use strict;),
+        q(use warnings;),
+        q(),
+        q(local ($|, $/) = (1, \1);),
+        q(my ($data, $si) = ('', 0);),
+        q(),
+    );
 
-    my @code;
-    my @loop;
-    while ($program =~ m{(([\Q><+-.,\E])(?:\2)*|.)}gsx) {
-        my $instr = substr $1, 0, 1;
+    BEGIN { ($hint_bits, $warning_bits, $hinthash) = ($^H, ${^WARNING_BITS}, \%^H) }
+    my $deparse = B::Deparse->new(q(-si0));
+    $deparse->ambient_pragmas(
+        hint_bits   => $hint_bits,
+        warning_bits=> $warning_bits,
+        q($[)       => 0 + $[,
+        q(%^H)      => $hinthash,
+    );
+
+    my $translate;
+    $translate = sub {
+        my ($indent, $ops) = @_;
+        for my $code (@{$ops}) {
+            my ($perl) = ($deparse->coderef2text($code) =~ m{^\{\s+(.+)\s+\}$}sx);
+            my $vars = PadWalker::closed_over($code);
+            $perl =~ s{\Q$_\E}{${$vars->{$_}}}gsx
+                for qw($WORD_SIZE $n);
+
+            if (q(ARRAY) eq ref $vars->{q{@sub}}) {
+                my @perl = split m{\n\r?}x, $perl;
+                push @buffer => (qq(\t) x $indent) . $perl[0];
+                $translate->($indent + 1, $vars->{q{@sub}});
+                push @buffer => (qq(\t) x $indent) . $perl[-1];
+            } else {
+                push @buffer => (qq(\t) x $indent) . $perl;
+            }
+        }
+    };
+    $translate->(0, $code);
+
+    return @buffer;
+}
+
+sub brainfuck {
+    my ($program, $WORD_SIZE) = @_;
+    $WORD_SIZE //= 8;
+
+    use bytes;
+    use integer;
+
+    my $to_perl = wantarray;
+
+    local ($|, $/) = (1, \1);
+    my ($data, $si, $int, $c) = ('', 0, 0, 0);
+
+    my ($start, %stats) = (Benchmark->new);
+    my $update_stats = sub {
+        @stats{qw{
+            vm_ticks
+            vm_mem
+            vm_time
+        }} = (
+            $c,
+            length $data,
+            timediff(Benchmark->new, $start)->timestr
+        );
+
+        return \%stats;
+    };
+    local $SIG{INT} = sub {
+        state $issued = 0;
+
+        if (time - $issued > 1) {
+            say STDERR Dumper $update_stats->();
+            $issued = time;
+        } else {
+            ++$int;
+        }
+    };
+
+    my (@code, @loop);
+    $program =~ s{[^><+\-\.,\[\]]+}{}gsx;
+    while ($program =~ m{(([><+\-])(?:\2)*|.)}gsx) {
         my $n = length $1;
 
-        given ($instr) {
+        given ($2 // $1) {
             when (q(>)) {
                 push @code
                     => $n == 1
@@ -44,18 +126,19 @@ sub brainfuck ($;$) {
                         : sub { vec($data, $si, $WORD_SIZE) -= $n }
             } when (q(.)) {
                 push @code
-                    => sub {
-                        print join ''
-                            => map
-                                { chr vec $data, $si, $WORD_SIZE }
-                                1 .. $n
-                    }
+                    => sub { print chr vec $data, $si, $WORD_SIZE }
             } when (q(,)) {
                 push @code
-                    => sub {
-                        vec($data, $si, $WORD_SIZE) = ord getc
-                            for 1 .. $n
-                    }
+                    => $to_perl
+                        ? sub { vec($data, $si, $WORD_SIZE) = ord getc }
+                        : sub {
+                            my $chr = getc;
+                            if (defined $chr) {
+                                vec($data, $si, $WORD_SIZE) = ord $chr;
+                            } else {
+                                ++$int;
+                            }
+                        }
             } when (q([)) {
                 push @loop => $#code
             } when (q(])) {
@@ -64,70 +147,39 @@ sub brainfuck ($;$) {
                 push @code
                     => sub {
                         while (vec $data, $si, $WORD_SIZE) {
-                            $_->() for @sub;
+                            for (@sub) {
+                                ++$c; $_->();
+                                return if $int;
+                            }
                         }
                     };
+                ++$stats{nested_level};
             }
         }
+
+        $stats{bf_count} += $n;
+        $stats{vm_count} ++;
     }
     confess q(unmatched '[') if @loop;
 
-    $_->() for @code;
+    if ($to_perl) {
+        return brainfuck2perl(\@code);
+    } else {
+        for (@code) {
+            ++$c; $_->();
+            last if $int;
+        }
 
-    return 1;
+        return $update_stats->();
+    }
 }
 
-use Benchmark;
+my $program;
 
-timethis 100 => sub {
-    # Hello World!
-    brainfuck
-        q(++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.);
+open(my $fh, q(<:raw), $ARGV[0])
+    or croak qq(Can't open: $!);
+{ local $/ = undef; $program = <$fh> }
+close $fh;
 
-    # 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
-    brainfuck << 'FIBONACCI';
-        +++++++++++
-        >+>>>>++++++++++++++++++++++++++++++++++++++++++++
-        >++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>
-        +<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-
-        <-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<
-        -]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]
-        >[<<+>>[-]]<<<<<<<]>>>>>[+++++++++++++++++++++++++
-        +++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++
-        ++++++++++++++++++++++++++++++++++++++++++++.[-]<<
-        <<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<
-        [-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]
-FIBONACCI
-    print "\n";
-};
-
-brainfuck << 'ROT13';
--,+[                        Read first character and start outer character reading loop
-    -[                      Skip forward if character is 0
-        >>++++[>++++++++<-] Set up divisor (32) for division loop
-                                (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
-        <+<-[               Set up dividend (x minus 1) and enter division loop
-            >+>+>-[>>>]     Increase copy and remainder / reduce divisor / Normal case: skip forward
-            <[[>+<-]>>+>]   Special case: move remainder back to divisor and increase quotient
-            <<<<<-          Decrement dividend
-        ]                   End division loop
-    ]>>>[-]+                End skip loop; zero former divisor and reuse space for a flag
-    >--[-[<->+++[-]]]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
-        ++++++++++++<[      If flag then set up divisor (13) for second division loop
-                                (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
-            >-[>+>>]        Reduce divisor; Normal case: increase remainder
-            >[+[<+>-]>+>>]  Special case: increase remainder / move it back to divisor / increase quotient
-            <<<<<-          Decrease dividend
-        ]                   End division loop
-        >>[<+>-]            Add remainder back to divisor to get a useful 13
-        >[                  Skip forward if quotient was 0
-            -[              Decrement quotient and skip forward if quotient was 1
-                -<<[-]>>    Zero quotient and divisor if quotient was 2
-            ]<<[<<->>-]>>   Zero divisor and subtract 13 from copy if quotient was 1
-        ]<<[<<+>>-]         Zero divisor and add 13 to copy if quotient was 0
-    ]                       End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
-    <[-]                    Clear remainder from first division if second division was skipped
-    <.[-]                   Output ROT13ed character from copy and clear it
-    <-,+                    Read next character
-]                           End character reading loop
-ROT13
+#say for brainfuck($program);
+brainfuck($program);
